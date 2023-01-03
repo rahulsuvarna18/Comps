645e66835262b1f465a7899154c8384df8a46477
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-01-03 13:42:01 +0530

    Added Tailwind.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Button from "./Button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Add To Cart!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
