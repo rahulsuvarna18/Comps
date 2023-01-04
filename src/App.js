efaa180c4c15ef3b5be51f50f76354621f9ff637
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-01-04 12:55:56 +0530

    made the button component accept event handlers and classnames

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { GoBell, GoCloudDownload, GoCode } from "react-icons/go";
 import Button from "./Button";
 
 function App() {
+  const handleClick = () => {
+    console.log("Clicked");
+  };
+
   return (
     <div>
       <div>
-        <Button success rounded outline>
+        <Button success rounded outline className="mb-5" onClick={handleClick}>
           <GoBell />
           Click me
         </Button>
